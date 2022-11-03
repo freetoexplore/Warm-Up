733a0117e0b67e08b951e1350f4b605f9e2789c7
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2022-11-03 23:39:42 +0800

    Add files via upload

--- HJ22_SparklingWater.py
+++ HJ22_SparklingWater.py
@@ -13,7 +13,7 @@ for start_number in input_list:
 
     # last turn
     # assume borrowing happens at the last turn when lender is running out of budget
-    # temperarily ignore on the borrowing amount as it will not be counted in the final return
+    # temperarily ignore on the borrowing amount as it is liability and will not counted in the final return 
     if 0 < start_n <= 3:
         borrow = 3 - start_n
         return_n.append(1)
@@ -22,3 +22,8 @@ for start_number in input_list:
 
 for output in final_output:
     print(output)
+
+# In this practice, I spent most of the time to consolidate the question into my comprehension,
+# which can be a lending issues.
+# The process taking the longest time, unexpectedly, is to reset the threshold of the 'out-of budget' scenario 
+# into the second tier.
