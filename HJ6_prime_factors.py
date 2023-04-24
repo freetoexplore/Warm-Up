edf2279d4cd79b6bdcd0e1ab6ba7c825081ae8be
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2023-04-24 19:08:21 +0800

    Update HJ6_prime_factors.py

--- HJ6_prime_factors.py
+++ HJ6_prime_factors.py
@@ -1,28 +1,49 @@
-## Ex. find all prime factors in an input number     
-# list all prime numbers < input number
-input_num = int(input()) # 2*10**9+14+1
+# 180
+# 64577
+# 2000000014
+input_int = int(input('Input: '))
 
-prime_in_range = []
-for num in range(1, input_num + 1):
-    n_factor = []
-    for factor in range(1, num + 1):
-        if num%factor == 0:
-            n_factor.append(factor)
-    if len(n_factor) == 2:
-        prime_in_range.append(factor)
+prime_flag = ''
+factors = []
 
-# divide input number by prime numbers in list
-prime_divisor = []
-for prime in prime_in_range:
-    if input_num%prime == 0:
-        prime_divisor.append(prime)
+for factor in range(1, input_int+1):
+    if input_int%factor == 0:
+        factors.append(factor)
         
+# prime number
+if len(factors) == 2:
+    prime_flag == 'Y'
+    print(input_int)
+        
+# not prime number
+else:
+    prime_flag = 'N'
+    
+    primes_factors = []
+    nums = [i for i in factors]
 
-# dvide by prime factors
-prime_list = ''
-for p in prime_divisor:
-    while input_num%p == 0:
-        prime_list += ' ' + str(p)
-        input_num = input_num/p
-
-print(prime_list.lstrip())
+    for num in nums:
+        factors = []
+        for factor in range(2, num + 1):
+            
+            if num % factor == 0:
+                factors.append(factor)
+             
+        if len(factors) == 1:
+            primes_factors.append(num) 
+            
+    print(primes_factors)    
+    final_factors = []
+    
+    for factor in primes_factors:
+        while input_int % factor == 0:
+            
+            input_int = input_int/factor
+            final_factors.append(factor)
+            
+    res_str = ''
+    
+    for i in final_factors:
+        res_str = (res_str + ' ' + str(i))
+    
+    print(res_str.lstrip())
