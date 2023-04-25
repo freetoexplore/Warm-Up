5197cda39436b94b663ae1d8f73905129311c587
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2023-04-25 17:05:27 +0800

    Update HJ6_prime_factors.py

--- HJ6_prime_factors.py
+++ HJ6_prime_factors.py
@@ -7,32 +7,30 @@ prime_flag = ''
 factors = []
 
 for factor in range(1, input_int+1):
+    prime_factors = []
     if input_int%factor == 0:
-        factors.append(factor)
         
-# prime number
-if len(factors) == 2:
-    prime_flag == 'Y'
-    print(input_int)
-        
-# not prime number
-else:
-    prime_flag = 'N'
-    
-    primes_factors = []
-    nums = [i for i in factors]
+# select prime factor
+        for pf in range(1, factor + 1):
+            if factor%pf == 0:
+                
+                prime_factors.append(pf)
+                prime_factors = [i for i in set(prime_factors)]
+                
+        if len(prime_factors) == 2:
+            
+            factors.append(factor)
+            
+final_factors = []            
+for final_factor in factors:
+    if input_int%final_factor == 0:
+        final_factors.append(final_factor)
+        input_int = input_int/final_factor
+           
+res_str = ''
 
-    for num in nums:
-        factors = []
-        for factor in range(2, num + 1):
-            
-            if num % factor == 0:
-                factors.append(factor)
-             
-        if len(factors) == 1:
-            primes_factors.append(num) 
-            
-    # print(primes_factors)    
+for num in final_factors:
+    res_str = res_str + ' ' + str(num) 
     final_factors = []
     
     for factor in primes_factors:
