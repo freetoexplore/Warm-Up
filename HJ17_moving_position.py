8e348afa7c1607ee7cbac8262c28d13302736cd4
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2023-04-29 05:00:24 +0800

    Update HJ17_moving_position.py
    *consider 1 digit steps in regex

--- HJ17_moving_position.py
+++ HJ17_moving_position.py
@@ -1,11 +1,11 @@
 import re
 
-input_pos = list(input('Please enter position list: ').split(';'))
+input_pos = list(input('Input list: ').split(';'))
 init_pos = [0, 0]
 
 for i in input_pos:
     
-    if re.match('\w{1}\d{2}$', i):
+    if re.match('\w{1}\d{1,2}$', i):
         direction = i[0]
         steps = int(i[1:])
         
@@ -14,11 +14,10 @@ for i in input_pos:
         
         if direction == 'D':
             init_pos[0] += steps
-            
         
         if direction == 'W':
             init_pos[1] += steps
-        
+
         if direction == 'S':
             init_pos[1] -= steps
         
