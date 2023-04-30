f9db2fd0ffd77c3fa2f3ed2a2bc0b90ae712fa72
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2023-04-30 22:14:29 +0800

    Update HJ16_shoplist.py
    pending for last case

--- HJ16_shoplist.py
+++ HJ16_shoplist.py
@@ -1,29 +1,30 @@
 import math
-input0 = '1000 5'# '50 5'# 
-input1 = '800 2 5' # '20 3 5'# 
-input2= '400 5 1' # '20 3 5'# 
-input3 = '300 5 1' # '10 3 0'# 
-input4 = '400 3 0' # '10 2 0'# 
-input5 = '500 2 0' # '10 1 0'# 
+input0 = str(input())
+budget = int(input0.split(' ')[0])
+n_constraint = int(input0.split(' ')[1])
 
 def extract_num(input_str, place):
     return int(input_str.split(' ')[place])
 
-prices = [extract_num(input1, 0), extract_num(input2, 0), extract_num(input3, 0), extract_num(input4, 0), extract_num(input5, 0)]
-values = [extract_num(input1, 1), extract_num(input2, 1), extract_num(input3, 1), extract_num(input4, 1), extract_num(input5, 1)]
-main_id = [extract_num(input1, 2), extract_num(input2, 2), extract_num(input3, 2), extract_num(input4, 2), extract_num(input5, 2)]
+prices = []
+values = []
+main_id = []
+
+for i in range(int(input0.split(' ')[1])):
+    intake_str = str(input())
+    prices += [extract_num(intake_str, 0) ]
+    values += [extract_num(intake_str, 1) ]
+    main_id +=[extract_num(intake_str, 2) ]
+
 main_id_clean = []
 
 ### start to script
-
 for i in main_id: 
     if i != 0:
         main_id_clean.append(i)
     else:
         main_id_clean.append(math.nan)
         
-print(main_id_clean)
-
 budget = int(input0.split(' ')[0])
 n_constraint = int(input0.split(' ')[1])
 scores_list = []
@@ -35,9 +36,9 @@ step_range_init = [i for i in range(n_constraint)]
 
 while round_turns < max_turns:
     scores = 0
-    print('round {0}'.format(round_turns))
+    # print('round {0}'.format(round_turns))
     step_range = step_range_init[round_turns:] + step_range_init[:round_turns]
-    print(step_range)
+    # print(step_range)
     
     step_list = []
     
@@ -54,7 +55,6 @@ while round_turns < max_turns:
             
             if budget >= 0 and n_constraint >= 0:
                 scores_list.append(scores)
-            print('main   step:', step, 'round:',round_turns,'score:',scores,'budget', budget, 'amount:', n_constraint, scores_list)
             
     # appendix part        
         else:
@@ -76,10 +76,7 @@ while round_turns < max_turns:
             
             if budget >= 0 and n_constraint >= 0:
                 scores_list.append(scores)
-                
-            print('append step', step, 'main step', step_main, 'round', round_turns,'score',scores, 'budget', budget, 'amount:', n_constraint, scores_list, 'step list:')
-                
-    print('round', round_turns, 'scores:', scores_list)        
+     
     round_turns += 1
     n_constraint = int(input0.split(' ')[1])
     budget = int(input0.split(' ')[0])
