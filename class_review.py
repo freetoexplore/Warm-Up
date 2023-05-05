1bf7d4db88cf08df5f01aaec8e1c08ab7d241b3c
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2023-05-05 09:38:24 +0800

    Update class_review.py

--- class_review.py
+++ class_review.py
@@ -1,25 +1,21 @@
-# this is an example from browser to resolve quadratic equation
+# this is an example from browser
 class Equation():
 
     def __init__(_):
-        
     # global parameters
-        _.a = int(input("Input 'a' for function f(x) = aX^2 + bX + c:")) # 1
-        _.b = int(input("Input 'b' for function f(x) = aX^2 + bX + c:")) # -5
-        _.c = int(input("Input 'c' for function f(x) = aX^2 + bX + c:")) # 6
-        _.g = 0 # root definition 求根公式定值，不需要输入覆盖
+        a = int(input("Input 'a' for function f(x) = aX^2 + bX + c:"))
+        b = int(input("Input 'b' for function f(x) = aX^2 + bX + c:"))
+        c = int(input("Input 'c' for function f(x) = aX^2 + bX + c:"))
+        g = 0 
     
     
     def Discriminants(_):
-        
     # initial object is referred by 'self' or '_'
-    # you may think it defined as a linkage from global parameter and called object
 
         _.g = _.b**2 - 4*_.a*_.c
-        print('求和公式(roots) =', _.b**2 - 4*_.a*_.c)
+        print('求和公式 =', _.b**2 - 4*_.a*_.c)
         
-    # Only global parameters are retrievable. Functions are not inter-callable. Because they served as sub-units under one class type.
-    # Keep in mind that defined objects are not reachable between defined functions.
+    
     def Root1(_):
         if _.b*_.b - 4*_.a*_.c >= 0:
             print((-_.b + _.g**0.5)/(2*_.a))
@@ -35,10 +31,11 @@ class Equation():
             print('No root for input formulation.')
             
             
-# call functions
+# Only global parameters are retrievable. Functions are not inter-callable. Because they served as sub-units under one class type.
+# Keep in mind that defined objects are not reachable between defined functions.
 def main():
     call_func = Equation()
-    call_func.Discriminants() # remember to call 求根公式(roots) to activate function _.g
+    call_func.Discriminants() 
     call_func.Root1()
     call_func.Root2()
 
